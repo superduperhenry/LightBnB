f4e2f6c297df95758e800de8736d3471a6a0dbc3
Henry Truong <henry.trb@gmail.com>
2023-05-17 22:34:12 -0400

    refactor getAllReservations to connect with db

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -66,7 +66,22 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT * 
+  FROM reservations
+  JOIN properties ON properties.id = property_id
+  JOIN users ON users.id = guest_id
+  WHERE guest_id = $1
+  LIMIT $2`;
+
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
