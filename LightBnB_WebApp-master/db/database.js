f263fd3a1da8637ec2e8ba88a9cec7203d6323db
Henry Truong <henry.trb@gmail.com>
2023-08-16 08:13:51 -0400

    update to inc conversion of dollars to cents on creation of listing

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -170,7 +170,7 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const {
+  let {
     owner_id,
     title,
     description,
@@ -186,6 +186,9 @@ const addProperty = function (property) {
     number_of_bathrooms,
     number_of_bedrooms,
   } = property;
+
+  //conversion of cost_per_night to cents
+  cost_per_night *= 100;
 
   const queryString = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10 , $11, $12, $13, $14)
   RETURNING *`;
